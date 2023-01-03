2d901ee95e4883381cf7f0c4a4679d332a9e0f20
brunodelucasbarbosa <bruno.lucas@ifood.com.br>
2023-01-03 12:08:37 -0300

    initial commit

---
+++ src/test/java/br/com/course/spring3rest/Spring3RestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course.spring3rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
